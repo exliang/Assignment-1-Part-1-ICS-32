1dcbbe9bce86a9ea907d7da79ff5b27f94c7be50
Emily Liang <exliang@uci.edu>
2024-01-20 12:43:59 -0800

    files then directories

--- a1p1.py
+++ a1p1.py
@@ -21,12 +21,20 @@ def user_input():
 	if command == 'L': #list contents of directory 
 		if len(command_list) == 2: #[COMMAND] [INPUT]
 			if any(myPath.iterdir()): #check if directory isnt empty
+				dir_list = []
+				file_list = []
 				for currentPath in myPath.iterdir(): #list contents of the directory 
-					print(currentPath) #FIXME: ensure your code properly sorts results into files first, followed by directories
+					if currentPath.isfile(): #if it's a file, put it in the file list
+						file_list.append(currentPath)
+					elif currentPath.isdir(): #if it's a directory, put it in the directory list 
+						dir_list.append(currentPath)
+				combined_list = file_list.extend(dir_list) #sorting results into files first, followed by directories
+				for directory in combined_list:
+					print(directory)
 		elif len(command_list) == 3: #[COMMAND] [INPUT] [[-]OPTION]
 			option = command_list[2]
 			#add more
-		elif len(command_list) == 4:
+		elif len(command_list) == 4: #[COMMAND] [INPUT] [[-]OPTION] [INPUT]
 			last_input = command_list[3] 
 
 	elif command == 'Q':
