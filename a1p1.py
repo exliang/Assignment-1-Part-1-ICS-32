0d22e6539dd92d734bbc63c2922cb68fb91c2d89
Emily Liang <exliang@uci.edu>
2024-01-19 17:16:20 -0800

    user input func outline

--- a1p1.py
+++ a1p1.py
@@ -1,6 +1,18 @@
 
-def main():
-	pass
+# a1p1.py
 
+# Emily Liang
+# exliang@uci.edu
+# 79453973
+
+def main():
+	user_input()
+
+def user_input():
+	command = input() #format: [COMMAND] [INPUT] [[-]OPTION] [INPUT]
+	if command[0] == 'L': #list contents of directory 
+		pass
+	elif command[0] == 'Q':
+		exit()
 if __name__ == '__main__':
 	main()
