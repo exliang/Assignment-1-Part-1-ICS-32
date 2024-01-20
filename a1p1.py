b63f1f57f0f08a761b6e67f63930baa4a9048180
Emily Liang <exliang@uci.edu>
2024-01-20 13:14:52 -0800

    list files only func

--- a1p1.py
+++ a1p1.py
@@ -20,25 +20,45 @@ def user_input():
 
 	if command == 'L': #list contents of directory 
 		if len(command_list) == 2: #[COMMAND] [INPUT]
-			if any(myPath.iterdir()): #check if directory isnt empty
-				dir_list = []
-				file_list = []
-				for currentPath in myPath.iterdir(): #list contents of the directory 
-					if currentPath.isfile(): #if it's a file, put it in the file list
-						file_list.append(currentPath)
-					elif currentPath.isdir(): #if it's a directory, put it in the directory list 
-						dir_list.append(currentPath)
-				combined_list = file_list.extend(dir_list) #sorting results into files first, followed by directories
-				for directory in combined_list:
-					print(directory)
+			list_directories(myPath)
 		elif len(command_list) == 3: #[COMMAND] [INPUT] [[-]OPTION]
 			option = command_list[2]
-			#add more
+			if option == '-r':
+				recursive_output()
+			elif option == '-f': #output files only
+				list_files(myPath)
+			elif option == '-s':
+				pass 
+			elif option == '-e':
+				pass 
 		elif len(command_list) == 4: #[COMMAND] [INPUT] [[-]OPTION] [INPUT]
 			last_input = command_list[3] 
-
+			#add more 
 	elif command == 'Q':
 		exit()
+
+def list_directories(myPath):
+	if any(myPath.iterdir()): #check if directory isnt empty
+		dir_list = []
+		file_list = []
+		for currentPath in myPath.iterdir(): #list contents of the directory 
+			if currentPath.is_file(): #if it's a file, put it in the file list
+				file_list.append(currentPath)
+			elif currentPath.is_dir(): #if it's a directory, put it in the directory list 
+				dir_list.append(currentPath)
+		file_list.extend(dir_list) #sorting results into files first, followed by directories
+		combined_list = file_list
+		for directory in combined_list:
+			print(directory)
+
+def list_files(myPath):
+	if any(myPath.iterdir()): #check if directory isnt empty
+		for currentPath in myPath.iterdir(): #list contents of the directory 
+			if currentPath.is_file():
+				print(currentPath)
+
+def recursive_output():
+	pass
 
 if __name__ == '__main__':
 	main()
