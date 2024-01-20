c7eff76e2045222866c249e7bc2d54ead0f65aa7
Emily Liang <exliang@uci.edu>
2024-01-20 15:09:40 -0800

    matching files func done

--- a1p1.py
+++ a1p1.py
@@ -5,6 +5,9 @@
 # exliang@uci.edu
 # 79453973
 
+#handle exception of a user requesting a folder that doesn't exist 
+#appropriate style --> change some code to camelcase (check lec doc)
+
 from pathlib import Path
 
 def main():
@@ -17,23 +20,25 @@ def user_input():
 	command = command_list[0]
 	path = command_list[1]
 	myPath = Path(path)
+	option = command_list[2]
 
 	if command == 'L': #list contents of directory 
 		if len(command_list) == 2: #[COMMAND] [INPUT]
 			list_directories(myPath)
 		elif len(command_list) == 3: #[COMMAND] [INPUT] [[-]OPTION]
-			option = command_list[2]
+			# option = command_list[2]
 			if option == '-r':
 				recursive_output()
 			elif option == '-f': #output files only
 				list_files(myPath)
-			elif option == '-s':
-				pass 
-			elif option == '-e':
-				pass 
 		elif len(command_list) == 4: #[COMMAND] [INPUT] [[-]OPTION] [INPUT]
-			last_input = command_list[3] 
-			#add more 
+			if option == '-s': #output files that match the file name
+				file_name = command_list[3] 
+				matching_files(myPath, file_name)
+			elif option == '-e':
+				extension = command_list[3]
+				matching_extension(myPath, file_name)
+
 	elif command == 'Q':
 		exit()
 
@@ -60,5 +65,16 @@ def list_files(myPath):
 def recursive_output():
 	pass
 
+def matching_files(myPath, file_name): 
+	for currentPath in myPath.iterdir(): #list contents of the directory 
+		if currentPath.is_file() and currentPath.name == file_name: #matching file name & must be a file
+			print(currentPath)
+
+def matching_extension(myPath, file_name):
+	pass
+
 if __name__ == '__main__':
 	main()
+
+# Citations:
+# - https://docs.python.org/3/library/pathlib.html
