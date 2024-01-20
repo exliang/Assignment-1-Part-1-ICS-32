65e8ea5235e707ef4353e8d1e1ddc1f78cca8853
Emily Liang <exliang@uci.edu>
2024-01-19 17:39:03 -0800

    iterdir

--- a1p1.py
+++ a1p1.py
@@ -5,14 +5,27 @@
 # exliang@uci.edu
 # 79453973
 
+from pathlib import Path
+
 def main():
 	user_input()
 
 def user_input():
-	command = input() #format: [COMMAND] [INPUT] [[-]OPTION] [INPUT]
-	if command[0] == 'L': #list contents of directory 
-		pass
-	elif command[0] == 'Q':
+	user_command = input() #format: [COMMAND] [INPUT] [[-]OPTION] [INPUT]
+	command_list = user_command.split()
+
+	command = command_list[0]
+	path = command_list[1]
+	option = command_list[2]
+	last_input = command_list[3]
+	myPath = Path(path)
+
+	if command == 'L': #list contents of directory 
+		#check if directory isnt empty
+			for currentPath in myPath.iterdir(): #list contents of the directory 
+				print(currentPath)
+	elif command == 'Q':
 		exit()
+
 if __name__ == '__main__':
 	main()
