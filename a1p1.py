6693b07f00cc38d46cde4ca008080d45b45107eb
Emily Liang <exliang@uci.edu>
2024-01-19 22:31:20 -0800

    if branches

--- a1p1.py
+++ a1p1.py
@@ -16,14 +16,19 @@ def user_input():
 
 	command = command_list[0]
 	path = command_list[1]
-	option = command_list[2]
-	last_input = command_list[3]
 	myPath = Path(path)
 
 	if command == 'L': #list contents of directory 
-		if any(myPath.iterdir()): #check if directory isnt empty
-			for currentPath in myPath.iterdir(): #list contents of the directory 
-				print(currentPath)
+		if len(command_list) == 2: #[COMMAND] [INPUT]
+			if any(myPath.iterdir()): #check if directory isnt empty
+				for currentPath in myPath.iterdir(): #list contents of the directory 
+					print(currentPath) #FIXME: ensure your code properly sorts results into files first, followed by directories
+		elif len(command_list) == 3: #[COMMAND] [INPUT] [[-]OPTION]
+			option = command_list[2]
+			#add more
+		elif len(command_list) == 4:
+			last_input = command_list[3] 
+
 	elif command == 'Q':
 		exit()
 
