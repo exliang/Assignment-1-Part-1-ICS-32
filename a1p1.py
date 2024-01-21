b91b66cd1d7d8ac526d03c9a2593746cb13f8ad3
Emily Liang <exliang@uci.edu>
2024-01-20 17:14:42 -0800

    checks if directory exists

--- a1p1.py
+++ a1p1.py
@@ -5,9 +5,6 @@
 # exliang@uci.edu
 # 79453973
 
-#handle exception of a user requesting a folder that doesn't exist 
-#appropriate style --> change some code to camelcase (check lec doc)
-
 from pathlib import Path
 
 def main():
@@ -20,27 +17,32 @@ def user_input():
 	command = command_list[0]
 	path = command_list[1]
 	myPath = Path(path)
-	option = command_list[2]
 
-	if command == 'L': #list contents of directory 
-		if len(command_list) == 2: #[COMMAND] [INPUT]
-			list_directories(myPath)
-		elif len(command_list) == 3: #[COMMAND] [INPUT] [[-]OPTION]
-			# option = command_list[2]
-			if option == '-r':
-				recursive_output()
-			elif option == '-f': #output files only
-				list_files(myPath)
-		elif len(command_list) == 4: #[COMMAND] [INPUT] [[-]OPTION] [INPUT]
-			if option == '-s': #output files that match the file name
-				file_name = command_list[3] 
-				matching_files(myPath, file_name)
-			elif option == '-e':
-				file_extension = command_list[3]
-				matching_extension(myPath, file_extension)
+	if myPath.exists(): #ensure that directory exists 
+		if command == 'L': #list contents of directory 
+			if len(command_list) == 2: #[COMMAND] [INPUT]
+				list_directories(myPath)
+			elif len(command_list) == 3: #[COMMAND] [INPUT] [[-]OPTION]
+				option = command_list[2]
+				if option == '-r':
+					recursive_output()
+				elif option == '-f': #output files only
+					list_files(myPath)
+			elif len(command_list) == 4: #[COMMAND] [INPUT] [[-]OPTION] [INPUT]
+				option = command_list[2]
+				if option == '-s': #output files that match the file name
+					file_name = command_list[3] 
+					if not file_name.exists(): #handling exception for if a file name doesn't exist 
+						idk = input("File does not exist")
+					matching_files(myPath, file_name)
+				elif option == '-e':
+					file_extension = command_list[3]
+					matching_extension(myPath, file_extension)
 
-	elif command == 'Q':
-		exit()
+		elif command == 'Q':
+			exit()
+	else:
+		print("Directory doesn't exist. Try again.")
 
 def list_directories(myPath):
 	if any(myPath.iterdir()): #check if directory isnt empty
@@ -62,9 +64,6 @@ def list_files(myPath):
 			if currentPath.is_file():
 				print(currentPath)
 
-def recursive_output():
-	
-
 def matching_files(myPath, file_name): 
 	for currentPath in myPath.iterdir(): #list contents of the directory 
 		if currentPath.is_file() and currentPath.name == file_name: #matching file name & must be a file
@@ -74,6 +73,9 @@ def matching_extension(myPath, file_extension):
 	for currentPath in myPath.iterdir(): #list contents of the directory 
 		if currentPath.name.endswith(file_extension): #check if the file type is the same as what the user entered 
 			print(currentPath)
+
+def recursive_output():
+	pass
 
 if __name__ == '__main__':
 	main()
